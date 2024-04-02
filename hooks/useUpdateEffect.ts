984dcff191dafccdd18fb8bdf2b19df847fb0ba8
Lystrillian <webeugene@proton.me>
2024-04-02 07:48:25 +0300

    feat(hooks): useUpdateEffect slightly refactored

--- hooks/useUpdateEffect.ts
+++ hooks/useUpdateEffect.ts
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useFirstMountState } from "./useFirstMountState";
+import { useEffect, useRef } from 'react'
+import { useFirstMountState } from './useFirstMountState'
 
 /**
  * A custom React hook that runs an effect function only when the component updates, after the initial render.
@@ -9,12 +9,16 @@ import { useFirstMountState } from "./useFirstMountState";
  */
 
 export const useUpdateEffect: typeof useEffect = (effect, deps = []) => {
-  const isFirstMount = useFirstMountState()
+  // Ref to store whether it's the first render or not
+  const isFirstMount = useRef(true)
 
   useEffect(() => {
-    if (!isFirstMount) {
+    // If it's not the first render, execute the effect function
+    if (!isFirstMount.current) {
       return effect()
     }
+    // Set the ref to false after the first render
+    isFirstMount.current = false
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [effect, isFirstMount, ...deps])
+  }, [effect, ...deps]) // Include effect and deps in the dependency array
 }
