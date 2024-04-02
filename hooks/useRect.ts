0c054497ec93d7c15067a7692cb51f628ca9d650
Lystrillian <webeugene@proton.me>
2024-04-02 18:34:53 +0300

    chore(docs): added docstrings and comments to useRect hook

--- hooks/useRect.ts
+++ hooks/useRect.ts
@@ -1,25 +1,41 @@
+/**
+ * This custom React hook, `useRect`, allows you to get the size and position of an HTML element.
+ * It uses the ResizeObserver API if available, otherwise it falls back to the window resize event.
+ *
+ * @param {MutableRefObject<any>} ref - The reference to the HTML element you want to observe.
+ * @returns {Object} - An object containing the size and position of the observed element.
+ */
+
 import { MutableRefObject, useCallback, useLayoutEffect, useState } from 'react'
 
 export const useRect = (ref: MutableRefObject<any>) => {
+  // Initialize the state with the initial size and position of the element
   const [rect, setRect] = useState(getRect(ref ? ref.current : null))
 
+  // Function to handle the resize event
   const handleResize = useCallback(() => {
+    // If the refference doesn't exist, return early
     if (!ref.current) {
       return
     }
 
-    // Update client rect
+    // Update client rect with the new size and position of the element
     setRect(getRect(ref.current))
   }, [ref])
 
+  // Effect hook to handle the resize event
   useLayoutEffect(() => {
     const element = ref.current
+
+    // If the refference is not available, return early
     if (!element) {
       return
     }
 
+    // Call the handleResize function to initialize the client rect
     handleResize()
 
+    // Check if the ResizeObserver API is available
     if (typeof ResizeObserver === 'function') {
       let resizeObserver: ResizeObserver | null = new ResizeObserver(() => handleResize())
       resizeObserver.observe(element)
