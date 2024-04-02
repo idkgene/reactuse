9b504cde8431e4b80f7a2fea844be0c0551e9c97
Lystrillian <webeugene@proton.me>
2024-04-02 08:39:29 +0300

    chore(layout): added ui for previewin all the hooks

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Dashboard() {
             <form className="grid w-full items-start gap-6">
               <fieldset className="grid gap-6 rounded-lg border p-4">
                 <legend className="-ml-1 px-1 text-sm font-medium">
-                  Block 1
+                  Hooks Block 1
                 </legend>
                 <div className="grid gap-3">
                   <Label htmlFor="useClipboard">useClipboard</Label>
@@ -151,6 +151,233 @@ export default function Dashboard() {
                     placeholder="Enter a new document title"
                   />
                   <p>Current document title: {title}</p>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useDrag">useDrag</Label>
+                  <div id="useDrag">This preview is under construction</div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
+            <form className="grid w-full items-start gap-6">
+              <fieldset className="grid gap-6 rounded-lg border p-4">
+                <legend className="-ml-1 px-1 text-sm font-medium">
+                  Hooks Block 2
+                </legend>
+                <div className="grid gap-3">
+                  <Label htmlFor="useEffectOnce">useEffectOnce</Label>
+                  <div id="useEffectOnce">
+                    This preview is under construction.
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useEventListener">useEventListener</Label>
+                  <div id="useEventListener">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useDebounce">useFavicon</Label>
+                  <div id="useFavicon">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useFetch">useFetch</Label>
+                  <div id="useFetch">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useFirstMountState">useFirstMountState</Label>
+                  <div id="useFirstMountState">
+                    This preview is under construction
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
+            <form className="grid w-full items-start gap-6">
+              <fieldset className="grid gap-6 rounded-lg border p-4">
+                <legend className="-ml-1 px-1 text-sm font-medium">
+                  Hooks Block 3
+                </legend>
+                <div className="grid gap-3">
+                  <Label htmlFor="useFouxFix">useFouxFix</Label>
+                  <div id="useFouxFix">This preview is under construction.</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useGeolocation">useGeolocation</Label>
+                  <div id="useGeolocation">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useHover">useHover</Label>
+                  <div id="useHover">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useIdle">useIdle</Label>
+                  <div id="useIdle">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useIntersectionObserver">
+                    useIntersectionObserver
+                  </Label>
+                  <div id="useIntersectionObserver">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useInterval">useInterval</Label>
+                  <div id="useInterval">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label id="useIOSToolbarState">useIOSToolbarState</Label>
+                  <div id="useIOSToolbarState">
+                    This preview is under construction
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
+            <form className="grid w-full items-start gap-6">
+              <fieldset className="grid gap-6 rounded-lg border p-4">
+                <legend className="-ml-1 px-1 text-sm font-medium">
+                  Hooks Block 4
+                </legend>
+                <div className="grid gap-3">
+                  <Label htmlFor="useFouxFix">useIsClient</Label>
+                  <div id="useFouxFix">This preview is under construction.</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useIsomorphicLayoutEffect">
+                    useIsomorphicLayoutEffect
+                  </Label>
+                  <div id="useIsomorphicLayoutEffect">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useIsTouchDevice">useIsTouchDevice</Label>
+                  <div id="useIsTouchDevice">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useIsVisible">useIsVisible</Label>
+                  <div id="useIsVisible">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useKeySequence">useKeySequence</Label>
+                  <div id="useKeySequence">
+                    This preview is under construction
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
+            <form className="grid w-full items-start gap-6">
+              <fieldset className="grid gap-6 rounded-lg border p-4">
+                <legend className="-ml-1 px-1 text-sm font-medium">
+                  Hooks Block 5
+                </legend>
+                <div className="grid gap-3">
+                  <Label htmlFor="useList">useList</Label>
+                  <div id="useList">This preview is under construction.</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useMediaQuery">useMediaQuery</Label>
+                  <div id="useMediaQuery">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useMousePosition">useMousePosition</Label>
+                  <div id="useMousePosition">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useNetworkState">useNetworkState</Label>
+                  <div id="useNetworkState">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useOnClickOutside">useOnCLickOutside</Label>
+                  <div id="useOnClickOutside">
+                    This preview is under construction
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
+            <form className="grid w-full items-start gap-6">
+              <fieldset className="grid gap-6 rounded-lg border p-4">
+                <legend className="-ml-1 px-1 text-sm font-medium">
+                  Hooks Block 6
+                </legend>
+                <div className="grid gap-3">
+                  <Label htmlFor="useOrientation">useOrientation</Label>
+                  <div id="useOrientation">
+                    This preview is under construction.
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="usePageLeave">usePageLeave</Label>
+                  <div id="usePageLeave">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useRect">useRect</Label>
+                  <div id="useRect">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useScript">useScript</Label>
+                  <div id="useScript">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useSessionStorage">useSessionStorage</Label>
+                  <div id="useSessionStorage">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useThrottle">useThrottle</Label>
+                  <div id="useThrottle">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useUnmount">useUnmount</Label>
+                  <div id="useUnmount">This preview is under construction</div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useUpdateEffect">useUpdateEffect</Label>
+                  <div id="useUpdateEffect">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useWindowLoad">useWindowLoad</Label>
+                  <div id="useWindowLoad">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useWindowResize">useWindowResize</Label>
+                  <div id="useWindowResize">
+                    This preview is under construction
+                  </div>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="useWindowSize">useWindowSize</Label>
+                  <div id="useWindowSize">
+                    This preview is under construction
+                  </div>
                 </div>
               </fieldset>
             </form>
