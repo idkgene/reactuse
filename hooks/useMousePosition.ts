92f77957a7bbc80c8fa9704219a701055853e513
Lystrillian <webeugene@proton.me>
2024-04-03 19:19:02 +0300

    chore(jsdoc): added jsdoc to useMousePosition

--- hooks/useMousePosition.ts
+++ hooks/useMousePosition.ts
@@ -1,5 +1,12 @@
 import { useEffect, useState } from 'react';
 
+
+/**
+ * @returns {Object} An object containing the current mouse position.
+ * @returns {number} position.x - The x-coordinate of the mouse position.
+ * @returns {number} position.y - The y-coordinate of the mouse position.
+ */
+
 const useMousePosition = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
