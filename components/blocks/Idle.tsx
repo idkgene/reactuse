097156373bcf5ae4898294be4ed2d759ca66adee
Lystrillian <webeugene@proton.me>
2024-04-03 13:15:27 +0300

    chore(organizatioon): threw useIdle ui to another file to free the main file

---
+++ components/blocks/Idle.tsx
@@ -0,0 +1,30 @@
+import { useIdle } from "../../hooks/useIdle"
+
+
+export default function IdleShowcase() {
+  const isIdle = useIdle(60000) // 1 minute
+
+  return (
+    <>
+     <div className="grid gap-3 p-4 border rounded-lg">
+                  <h2
+                    id="useIdle"
+                    className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    useIdle
+                  </h2>
+                  <div>
+                    {isIdle ? (
+                      <p className="mt-3 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                        🦥 User is idle
+                      </p>
+                    ) : (
+                      <p className="mt-3 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                        🙋🏻‍♂️ User is active
+                      </p>
+                    )}
+                  </div>
+                </div>
+    </>
+  )
+}
