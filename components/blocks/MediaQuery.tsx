36fd01c85222fe3628be560eb1209bab72eceb0e
Lystrillian <webeugene@proton.me>
2024-04-04 13:21:36 +0300

    feat(ui): added mediaquery preview to showcase

--- components/blocks/MediaQuery.tsx
+++ components/blocks/MediaQuery.tsx
@@ -1,6 +1,12 @@
-import Alert from '@ui-components/alert'
+import { useMediaQuery } from '@hooks/useMediaQuery'
 
 export default function MediaQueryShowcase() {
+  const isSmallScreen = useMediaQuery('(max-width: 767px)')
+  const isMediumScreen = useMediaQuery(
+    '(min-width: 768px) and (max-width: 1023px)',
+  )
+  const isLargeScreen = useMediaQuery('(min-width: 1024px)')
+
   return (
     <>
       <div className="grid gap-3 p-4 border rounded-lg">
@@ -10,10 +16,11 @@ export default function MediaQueryShowcase() {
         >
           useMediaQuery
         </h2>
-        <Alert
-          id="useMediaQuery"
-          message="This preview is under construction."
-        />
+        <div>
+          {isSmallScreen && <p>You are on a small screen.</p>}
+          {isMediumScreen && <p>You are on a medium screen.</p>}
+          {isLargeScreen && <p>You are on a large screen.</p>}
+        </div>
       </div>
     </>
   )
