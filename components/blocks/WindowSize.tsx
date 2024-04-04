4c862da789c49a3743a682e6b346aafdcb43c6b3
Lystrillian <webeugene@proton.me>
2024-04-04 13:22:26 +0300

    chore: removed unneccessary line

--- components/blocks/WindowSize.tsx
+++ components/blocks/WindowSize.tsx
@@ -1,11 +1,9 @@
 import { useWindowSize } from '@/hooks/useWindowSize'
-import Heading from '../hedaings/heading'
 export default function WindowSizeShowcase() {
   const windowSize = useWindowSize()
 
   return (
     <div className="grid gap-3 p-4 border rounded-lg">
-      <Heading level={6}>Some Text</Heading>
       <h2
         id="useWindowSize"
         className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
