168e0df47303d454ad858290c9057d7a624a8544
Lystrillian <webeugene@proton.me>
2024-04-11 14:43:08 +0300

    documentready reorganized

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import {
   TooltipTrigger,
 } from "@radix-ui/react-tooltip";
 import {
-  DocumentReadyShowCase,
   DocumentTitleShowcase,
   DragShowcase,
   FaviconShowcase,
@@ -48,6 +47,7 @@ import ClientShowcase from "@/components/Containers/Client";
 import ClipboardShowcase from "@/components/Containers/Clipboard";
 import DebounceShowcase from "@/components/Containers/Debounce";
 import DebugShowcase from "@/components/Containers/Debug";
+import DocumentReadyShowCase from "@/components/Containers/DocumentReady";
 
 export default function Dashboard() {
   return (
--- components/blocks/Showcases.tsx
+++ components/blocks/Showcases.tsx
@@ -1,46 +1,38 @@
-import MediaQueryShowcase from '@/components/blocks/MediaQuery'
-import MousePositionShowcase from '@/components/blocks/MousePosition'
-import NetworkState from '@/components/blocks/NetworkState'
-import ClientShowcase from '@ui-showcase/Client'
-import ClipboardShowcase from '@ui-showcase/Clipboard'
-import DebounceShowcase from '@ui-showcase/Debounce'
-import DebugShowcase from '@ui-showcase/Debug'
-import DocumentReadyShowCase from '@ui-showcase/DocumentReady'
-import DocumentTitleShowcase from '@ui-showcase/DocumentTitle'
-import DragShowcase from '@ui-showcase/Drag'
-import FaviconShowcase from '@ui-showcase/Favicon'
-import FetchShowcase from '@ui-showcase/Fetch'
-import FirstMountStateShowcase from '@ui-showcase/FirstMountState'
-import GeolocationShowcase from '@ui-showcase/Geolocation'
-import HooksBlock from '@ui-showcase/HooksBlock'
-import HoverShowase from '@ui-showcase/Hover'
-import IOSToolbarStateShowcase from '@ui-showcase/IOSToolbarState'
-import IntersectionObserverShowcase from '@ui-showcase/IntersectionObserver'
-import IntervalShowcase from '@ui-showcase/Interval'
-import IsTouchDeviceShowcase from '@ui-showcase/IsTouchDevice'
-import IsVisibleShowcase from '@ui-showcase/IsVisible'
-import IsomorphicLayoutEffect from '@ui-showcase/IsomorphicLayoutEffect'
-import KeySequenceShowcase from '@ui-showcase/KeySequence'
-import ListShowcase from '@ui-showcase/List'
-import OnClickOutsideShowcase from '@ui-showcase/OnClickOutside'
-import OrientationShowcase from '@ui-showcase/Orientation'
-import PageLeaveShowcase from '@ui-showcase/PageLeave'
-import RectShowcase from '@ui-showcase/Rect'
-import ScriptShowcase from '@ui-showcase/Script'
-import SessioStorageShowcase from '@ui-showcase/SessionStorage'
-import ThrottleShowcase from '@ui-showcase/Throttle'
-import UnmountShowcase from '@ui-showcase/Unmount'
-import UpdateEffectShowcase from '@ui-showcase/UpdateEffect'
-import UseEffectOnceShowcase from '@ui-showcase/UseEffectOnce'
-import WindowLoadShowcase from '@ui-showcase/WindowLoad'
-import WindowResizeShowcase from '@ui-showcase/WindowResize'
-import WindowSizeShowcase from '@ui-showcase/WindowSize'
+import MediaQueryShowcase from "@/components/blocks/MediaQuery";
+import MousePositionShowcase from "@/components/blocks/MousePosition";
+import NetworkState from "@/components/blocks/NetworkState";
+import DocumentReadyShowCase from "@ui-showcase/DocumentReady";
+import DocumentTitleShowcase from "@ui-showcase/DocumentTitle";
+import DragShowcase from "@ui-showcase/Drag";
+import FaviconShowcase from "@ui-showcase/Favicon";
+import FetchShowcase from "@ui-showcase/Fetch";
+import FirstMountStateShowcase from "@ui-showcase/FirstMountState";
+import GeolocationShowcase from "@ui-showcase/Geolocation";
+import HooksBlock from "@ui-showcase/HooksBlock";
+import HoverShowase from "@ui-showcase/Hover";
+import IOSToolbarStateShowcase from "@ui-showcase/IOSToolbarState";
+import IntersectionObserverShowcase from "@ui-showcase/IntersectionObserver";
+import IntervalShowcase from "@ui-showcase/Interval";
+import IsTouchDeviceShowcase from "@ui-showcase/IsTouchDevice";
+import IsVisibleShowcase from "@ui-showcase/IsVisible";
+import IsomorphicLayoutEffect from "@ui-showcase/IsomorphicLayoutEffect";
+import KeySequenceShowcase from "@ui-showcase/KeySequence";
+import ListShowcase from "@ui-showcase/List";
+import OnClickOutsideShowcase from "@ui-showcase/OnClickOutside";
+import OrientationShowcase from "@ui-showcase/Orientation";
+import PageLeaveShowcase from "@ui-showcase/PageLeave";
+import RectShowcase from "@ui-showcase/Rect";
+import ScriptShowcase from "@ui-showcase/Script";
+import SessioStorageShowcase from "@ui-showcase/SessionStorage";
+import ThrottleShowcase from "@ui-showcase/Throttle";
+import UnmountShowcase from "@ui-showcase/Unmount";
+import UpdateEffectShowcase from "@ui-showcase/UpdateEffect";
+import UseEffectOnceShowcase from "@ui-showcase/UseEffectOnce";
+import WindowLoadShowcase from "@ui-showcase/WindowLoad";
+import WindowResizeShowcase from "@ui-showcase/WindowResize";
+import WindowSizeShowcase from "@ui-showcase/WindowSize";
 
 export {
-  ClientShowcase,
-  ClipboardShowcase,
-  DebounceShowcase,
-  DebugShowcase,
   DocumentReadyShowCase,
   DocumentTitleShowcase,
   DragShowcase,
@@ -74,4 +66,4 @@ export {
   WindowLoadShowcase,
   WindowResizeShowcase,
   WindowSizeShowcase,
-}
+};
