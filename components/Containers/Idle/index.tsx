e8bcdb4e1317dbc42a5776eae39c66ae64cc5354
Lystrillian <webeugene@proton.me>
2024-04-11 15:15:14 +0300

    idle reorganized

---
+++ components/Containers/Idle/index.tsx
@@ -0,0 +1,15 @@
+import { useIdle } from "@hooks/useIdle";
+import styles from "./index.module.css";
+
+export default function IdleShowcase() {
+  const isIdle = useIdle(60000); // 1 minute
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h2 id="useIdle">useIdle</h2>
+        <div>{isIdle ? <p>🦥 User is idle</p> : <p>🙋🏻‍♂️ User is active</p>}</div>
+      </div>
+    </>
+  );
+}
