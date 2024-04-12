35946b9fa7d57fdcafb27156b64ce8caf6bdfdc1
Lystrillian <webeugene@proton.me>
2024-04-12 11:23:00 +0300

    windowresize removed

--- components/blocks/Showcases.tsx
+++ components/blocks/Showcases.tsx
@@ -1,9 +1,7 @@
 import HooksBlock from "@ui-showcase/HooksBlock";
-import WindowResizeShowcase from "@ui-showcase/WindowResize";
 import WindowSizeShowcase from "@ui-showcase/WindowSize";
 
 export {
   HooksBlock,
-  WindowResizeShowcase,
   WindowSizeShowcase,
 };
