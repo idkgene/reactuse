186a786fe76d3eba9f0d4b99c529805fecf89cd5
Lystrillian <webeugene@proton.me>
2024-04-12 11:00:29 +0300

    onClickOutside reorganized

---
+++ components/Containers/OnClickOutside/index.tsx
@@ -0,0 +1,16 @@
+import Alert from "@ui-components/alert";
+import styles from "./index.module.css";
+
+export default function OnClickOutsideShowcase() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2 id="useOnClickOutside">useOnCLickOutside</h2>
+        <Alert
+          id="useOnCLickOutside"
+          message="This preview is under construction."
+        />
+      </div>
+    </>
+  );
+}
