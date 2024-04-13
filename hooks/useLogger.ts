4cf8cd14cd166dec26c8cdf1183a091422cef4a5
Lystrillian <webeugene@proton.me>
2024-04-13 09:49:21 +0300

    useLogger refactored & checked for null pointers

--- hooks/useLogger.ts
+++ hooks/useLogger.ts
@@ -1,17 +1,28 @@
 import { useEffect, useRef } from "react";
 
-export const useLogger = (name, ...args) => {
-  const componentsName = useRef(name);
+type LoggerArgs = any[];
+
+/**
+ * A custom hook that logs the mount and unmount events of a component.
+ * @param name
+ * @param args
+ */
+export const useLogger = <T>(name: T, ...args: LoggerArgs) => {
+  if (name === null) {
+    throw new Error("The name argument cannot be null or undefined");
+  }
+
+  const componentsName = useRef<T>(name);
 
   useEffect(() => {
-    console.log(`[${componentsName.current}] Mounted`, ...args);
+    console.log(`[${componentsName.current}] Mounted`, ...(args || []));
 
     return () => {
-      console.log(`[${componentsName.current}] Unmounted`, ...args);
+      console.log(`[${componentsName.current}] Unmounted`, ...(args || []));
     };
-  }, []);
+  }, [componentsName, args]);
 
   useEffect(() => {
-    console.log(`[${componentsName.current}] Updated`, ...args);
-  });
+    console.log(`[${componentsName.current}] Updated`, ...(args || []));
+  }, [componentsName, args]);
 };
