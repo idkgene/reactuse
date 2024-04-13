25f8f8f5fd33dd6ba84025bf58245182203bc136
Lystrillian <webeugene@proton.me>
2024-04-13 10:06:20 +0300

    added usePrefferedLanguage hook

---
+++ hooks/usePrefferedLanguage.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+type LanguageCode = string;
+
+/**
+ * A custom hook that returns the preferred language code of the browser.
+ * @returns
+ */
+const usePreferredLanguage = (): LanguageCode => {
+  const [preferredLanguage, setPreferredLanguage] =
+    useState<LanguageCode>("en");
+
+  useEffect(() => {
+    const getPreferredLanguage = (): LanguageCode => {
+      if (typeof window !== "undefined" && window.navigator) {
+        const language = window.navigator.language;
+        return language.split("-")[0];
+      }
+      return "en";
+    };
+
+    const handleLanguageChange = () => {
+      setPreferredLanguage(getPreferredLanguage());
+    };
+
+    handleLanguageChange();
+
+    window.addEventListener("languagechange", handleLanguageChange);
+
+    return () => {
+      window.removeEventListener("languagechange", handleLanguageChange);
+    };
+  }, []);
+
+  return preferredLanguage;
+};
+
+export default usePreferredLanguage;
