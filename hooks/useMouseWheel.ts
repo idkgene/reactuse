a2c68c8d91980f11e08dc6d7872b0c3a0fd67186
Lystrillian <webeugene@proton.me>
2024-04-13 10:39:27 +0300

    commented usemousewheel hook

--- hooks/useMouseWheel.ts
+++ hooks/useMouseWheel.ts
@@ -1,9 +1,18 @@
 import { useState, useEffect } from "react";
 
+/**
+ * A custom React hook that returns the mouse scroll delta Y
+ * @returns {number} The mouse scroll delta Y
+ */
 const useMouseScrollDeltaY = (): number => {
   const [deltaY, setDeltaY] = useState<number>(0);
 
   useEffect(() => {
+    /**
+     * Handles the wheel event and updates the deltaY state.
+     *
+     * @param {WheelEvent} event - The wheel event object.
+     */
     const handleWheel = (event: WheelEvent) => {
       setDeltaY(event.deltaY);
     };
