955ee94668ecad1125a0b0c78e749324bba936c7
Lystrillian <webeugene@proton.me>
2024-05-01 17:35:01 +0300

    jsdoces useMeasure

--- hooks/useMeasure.ts
+++ hooks/useMeasure.ts
@@ -1,11 +1,21 @@
 import { useState, useEffect, useRef } from "react";
 
+/**
+ * @type UseMeasureReturn
+ * @property {number} height - The height of the element.
+ * @property {number} width - The width of the element.
+ * @property {React.RefObject<HTMLElement | null>} ref - A reference to the element.
+ */
 type UseMeasureReturn = {
   height: number;
   width: number;
   ref: React.RefObject<HTMLElement | null>;
 };
 
+/**
+ * A custom hook that returns the height and width of an element, as well as a reference to the element.
+ * @returns {UseMeasureReturn} An object containing the height and width of the element, as well as a reference to the element.
+ */
 const useMeasure = (): UseMeasureReturn => {
   const [height, setHeight] = useState(0);
   const [width, setWidth] = useState(0);
@@ -25,11 +35,21 @@ const useMeasure = (): UseMeasureReturn => {
   };
 };
 
+/**
+ * @type UseSizeReturn
+ * @property {number} height - The height of the element.
+ * @property {number} width - The width of the element.
+ * @property {React.RefObject<HTMLElement | null>} ref - A reference to the element.
+ */
 type UseSizeReturn = {
   size: { height: number; width: number };
   ref: React.RefObject<HTMLElement | null>;
 };
 
+/**
+ * A custom hook that returns the size of an element, as well as a reference to the element.
+ * @returns {UseSizeReturn} An object containing the size of the element, as well as a reference to the element.
+ */
 const useSize = (): UseSizeReturn => {
   const [size, setSize] = useState<{ height: number; width: number }>({
     height: 0,
