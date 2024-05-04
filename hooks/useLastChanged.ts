283b2f14c6a8702b694c6728c390200a84065105
Lystrillian <webeugene@proton.me>
2024-05-04 14:09:44 +0300

    commented useLastChanged

--- hooks/useLastChanged.ts
+++ hooks/useLastChanged.ts
@@ -4,6 +4,13 @@ interface UseLastChangedOptions {
   initialValue?: number;
 }
 
+/**
+ * A custom React hook that returns a reference to the last time a value was changed.
+ * @template T - The type of the value.
+ * @param {T} source - The value to check for changes.
+ * @param {UseLastChangedOptions} [options] - Options for the hook.
+ * @returns {React.MutableRefObject<number | null>} A reference to the last time the value was changed.
+ */
 export function useLastChanged<T>(
   source: T,
   options: UseLastChangedOptions = {},
