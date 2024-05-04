933a00a7f121f0e3fb3d32b1ea4f6f6bdf8f22d7
Lystrillian <webeugene@proton.me>
2024-05-04 11:41:18 +0300

    jsdoced useTrunc

--- hooks/useTrunc.ts
+++ hooks/useTrunc.ts
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+/**
+ * A custom React hook that returns a truncated number.
+ * @param {number} number - The number to be truncated.
+ * @returns {[number | null, (number: number) => void]} An array containing the truncated number and a function to truncate the number.
+ */
 const useTrunc = () => {
   const [result, setResult] = useState<number | null>(null);
 
