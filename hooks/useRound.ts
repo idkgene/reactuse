312958e40b0e653c8b5bd93067c3adc45ea41f03
Lystrillian <webeugene@proton.me>
2024-05-04 11:46:55 +0300

    refactored useRound

--- hooks/useRound.ts
+++ hooks/useRound.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 /**
  * A custom React hook that returns a rounded number.
@@ -6,13 +6,17 @@ import { useState } from "react";
  * @returns {[number | null, (number: number) => void]} An array containing the rounded number and a function to round the number.
  */
 const useRound = () => {
-  const [result, setResult] = useState<number | null>(null);
+  const [result, setResult] = useState<number>(0);
 
-  const roundNumber = (number: number) => {
+  const roundNumber = useCallback((number: number) => {
+    if (typeof number !== "number") {
+      console.error("useRound: Input must be a number");
+      return;
+    }
     setResult(Math.round(number));
-  }
+  }, []);
 
   return [result, roundNumber];
-}
+};
 
-export default useRound
+export { useRound };
