27cf5278f87526bdbe652510ae5dd2e00c6f74ea
Lystrillian <webeugene@proton.me>
2024-05-04 12:00:23 +0300

    useMax commented

--- hooks/useMax.ts
+++ hooks/useMax.ts
@@ -1,5 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
 
+/**
+ * A custom React hook that returns the maximum value.
+ * @param {number[]} numbers - An array of numbers.
+ * @returns {[number, (newNumbers: number[]) => void]} An array containing the maximum value and a function to update the maximum value.
+ */
 const useMax = (numbers: number[]) => {
   const [max, setMax] = useState<number>(Math.max(...numbers));
 
