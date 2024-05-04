bc44d777b852e6e92e09c617aa7f6a695c6257bb
Lystrillian <webeugene@proton.me>
2024-05-04 11:56:03 +0300

    usePrecision commented

--- hooks/usePrecision.ts
+++ hooks/usePrecision.ts
@@ -5,12 +5,23 @@ type PrecisionOptions = {
   value: number;
 };
 
+/**
+ * @function formatNumber
+ * @param {number} value - The value to be formatted.
+ * @param {number} precision - The precision of the formatted value.
+ * @returns {number} The formatted value.
+ */
 const formatNumber = (value: number, precision: number) => {
   return Number(value.toFixed(precision));
 };
 
+/**
+ * A custom React hook that sets the precision of a number.
+ * @param {PrecisionOptions} { precision, value } - The precision and value to be formatted.
+ * @returns {number} The formatted value.
+ */
 const usePrecision = ({ precision, value }: PrecisionOptions) => {
-  const [formattedValue, setFormattedValue] = useState(
+  const [formattedValue, setFormattedValue] = useState<number>(
     formatNumber(value, precision),
   );
 
