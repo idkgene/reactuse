0cd74463f07c947b37e64f091f412c48b9d25442
Lystrillian <webeugene@proton.me>
2024-05-04 12:26:51 +0300

    commented useAbs

--- hooks/useAbs.ts
+++ hooks/useAbs.ts
@@ -2,6 +2,11 @@ import { useCallback, useEffect, useState } from "react";
 
 type MaybeRefOrGetter<T> = T | (() => T);
 
+/**
+ * A custom React hook that returns the absolute value of a number.
+ * @param {MaybeRefOrGetter<number>} value - The number to get the absolute value of.
+ * @returns {number} The absolute value of the number.
+ */
 export const useAbs = (value: MaybeRefOrGetter<number>) => {
   const [absValue, setAbsValue] = useState(() =>
     Math.abs(typeof value === "function" ? value() : value),
