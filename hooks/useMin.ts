87796b68d04e94e6089d1fd9045347fe4e6d5efd
Lystrillian <webeugene@proton.me>
2024-05-04 11:58:26 +0300

    useMin commented

--- hooks/useMin.ts
+++ hooks/useMin.ts
@@ -1,5 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
 
+/**
+ * A custom React hook that returns the minimum value.
+ * @param {number[]} numbers - An array of numbers.
+ * @returns {[number, (newNumbers: number[]) => void]} An array containing the minimum value and a function to update the minimum value.
+ */
 const useMin = (numbers: number[]) => {
   const [min, setMin] = useState<number>(Math.min(...numbers));
 
