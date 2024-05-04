61c54cab26076d7bf9b7f68636c6495a837d1abb
Lystrillian <webeugene@proton.me>
2024-05-04 12:23:09 +0300

    logicOr commented

--- hooks/logicOr.ts
+++ hooks/logicOr.ts
@@ -2,6 +2,11 @@ import { useEffect, useState, useCallback } from "react";
 
 type MaybeRefOrGetter<T> = T | (() => T);
 
+/**
+ * A custom React hook that returns a boolean indicating if any of the provided refs or getters are true.
+ * @param {...MaybeRefOrGetter<boolean>} args - The refs or getters to be ORed together.
+ * @returns {[boolean, (newValues: boolean[]) => void]} An array containing the result of the OR operation and a function to update the values.
+ */
 export const logicOr = (
   ...args: MaybeRefOrGetter<boolean>[]
 ): [boolean, (newValues: boolean[]) => void] => {
