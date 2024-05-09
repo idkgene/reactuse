269654502c1c5eac17ffcde9601caba24834090b
Lystrillian <webeugene@proton.me>
2024-05-09 14:55:17 +0300

    created robots file (links needed to be changed on prod)

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: "https://exaxmple.com/sitemap.xml",
+    host: "https://example.com",
+  };
+}
