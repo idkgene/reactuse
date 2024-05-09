774ca9a62ce9b0778441472d039d8179df66e1c2
Lystrillian <webeugene@proton.me>
2024-05-09 14:53:24 +0300

    added fallback fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,27 +1,34 @@
 import localFont from "next/font/local";
+import Roboto from "next/font/google";
 
 const WixMadeforDisplayExtraBold = localFont({
   src: "../public/fonts/WixMadeforDisplay-ExtraBold.ttf",
+  fallback: ["system-ui", "Roboto"],
 });
 
 const WixMadeforDisplayBold = localFont({
   src: "../public/fonts/WixMadeforDisplay-Bold.ttf",
+  fallback: ["system-ui", "Roboto"],
 });
 
 const WixMadeforDisplaySemiBold = localFont({
   src: "../public/fonts/WixMadeforDisplay-SemiBold.ttf",
+  fallback: ["system-ui", "Roboto"],
 });
 
 const WixMadeforDisplayMedium = localFont({
   src: "../public/fonts/WixMadeforDisplay-Medium.ttf",
+  fallback: ["system-ui", "Roboto"],
 });
 
 const WixMadeforDisplayRegular = localFont({
   src: "../public/fonts/WixMadeforDisplay-Regular.ttf",
+  fallback: ["system-ui", "Roboto"],
 });
 
 const WixMadeForDisplayVariable = localFont({
   src: "../public/fonts/WixMadeforDisplay-VariableFont_wght.ttf",
+  fallback: ["system-ui", "Roboto"],
 });
 
 export {
