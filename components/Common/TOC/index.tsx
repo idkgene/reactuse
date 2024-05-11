568f167fef7e9757ff7e560776fa452ec674e58f
Lystrillian <webeugene@proton.me>
2024-05-11 12:10:45 +0300

    table of contents component refactored

--- components/Common/TOC/index.tsx
+++ components/Common/TOC/index.tsx
@@ -1,63 +1,76 @@
-"use client";
-import { useEffect, useRef } from "react";
+// TableOfContents.tsx
+import { useEffect, useState } from "react";
+import * as React from "react";
+
+interface Heading {
+  text: string;
+  level: number;
+  id: string;
+}
 
 interface TableOfContentsProps {
-  headers: HTMLHeadingElement[];
+  contentId: string;
 }
 
-export default function TableOfContents({ headers }: TableOfContentsProps) {
-  const tocRef = useRef<HTMLDivElement>(null);
+export const TableOfContents: React.FC<TableOfContentsProps> = ({
+  contentId,
+}) => {
+  const [headings, setHeadings] = useState<Heading[]>([]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const id = entry.target.id;
-            const link = tocRef.current?.querySelector(
-              `[href="#${id}"]`
-            ) as HTMLAnchorElement;
-            if (link) {
-              link.classList.add("active");
-            }
-          } else {
-            const link = tocRef.current?.querySelector(
-              `.active[href="#${entry.target.id}"]`
-            ) as HTMLAnchorElement;
-            if (link) {
-              link.classList.remove("active");
-            }
-          }
-        });
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 1.0,
-      }
-    );
-
-    headers.forEach((header) => {
-      observer.observe(header);
-    });
-
-    return () => {
-      headers.forEach((header) => {
-        observer.unobserve(header);
-      });
-    };
-  }, [headers]);
+    const contentElement = document.getElementById(contentId);
+    if (contentElement) {
+      const headers = Array.from(
+        contentElement.querySelectorAll<HTMLHeadingElement>(
+          "h2, h3, h4, h5, h6"
+        )
+      );
+      const headingData: Heading[] = headers
+        .map((header) => ({
+          text: header.textContent ?? "",
+          level: parseInt(header.tagName.slice(1)),
+          id: header.id,
+        }))
+        .filter((heading) => heading.text.trim() !== "");
+      setHeadings(headingData);
+    }
+  }, [contentId]);
 
   return (
-    <div ref={tocRef} className="table-of-contents">
-      {headers.map((header, index) => {
-        const slug = header.textContent?.toLowerCase().replace(/\s+/g, "-");
-        return (
-          <a key={index} href={`#${slug}`}>
-            {header.textContent}
-          </a>
-        );
-      })}
+    <div className="table-of-contents relative hidden order-2 grow pl-8 w-full max-w-[256px] xl:block">
+      <div id="curtain" className="fixed top-0 pt-[128px]"></div>
+      <div
+        id="aside-container"
+        className="fixed top-0 pt-[128px] w-[224px] h-full overflow-x-hidden overflow-y-hidden"
+      >
+        <div
+          id="aside-content"
+          className="flex flex-col pb-8 min-h-[calc(100vh-(64px+var(--vp-layout-top-height,0px)+48px))]"
+        >
+          <div
+            id="asideOutline has-outline"
+            role="navigation"
+            className="flex flex-col grow"
+          >
+            <div id="content" className="relative pl-4 text-[13px] font-medium">
+              <h2 className="text-[14px] font-semibold leading-[32px]">
+                On this page
+              </h2>
+              {headings.map((heading) => (
+                <li key={heading.id} className="list-none">
+                  <a
+                    href={`#${heading.id}`}
+                    className="leading-[32px] text-[14px] transition-colors text-ellipsis overflow-hidden whitespace-nowrap text-[rgba(235,235,245,.6)] hover:text-white"
+                  >
+                    {heading.text}
+                  </a>
+                </li>
+              ))}
+            </div>
+          </div>
+          <div id="spacer" className="grow"></div>
+        </div>
+      </div>
     </div>
   );
-}
+};
