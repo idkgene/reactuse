b4c1aa84d8dc0ef3602c9c38742a0910ca81bede
Lystrillian <webeugene@proton.me>
2024-05-11 12:11:07 +0300

    removed unnecessary aria-checked in toggle component

--- components/Common/Toggle/index.tsx
+++ components/Common/Toggle/index.tsx
@@ -1,23 +1,23 @@
-import { Sun, Moon } from "lucide-react";
-import styles from "./index.module.css";
+import { Sun, Moon } from 'lucide-react'
+import styles from './index.module.css'
 
 const ThemeToggle = () => {
-  return (
-    <div id="appearence" className={styles.content}>
-      <button type="button" id="theme" aria-checked="false">
-        <span className={styles.check}>
-          <span className={styles.circle}>
-            <span className={styles.iconInactive}>
-              <Sun size={12} />
-            </span>
-            <span className={styles.icon}>
-              <Moon size={12} />
-            </span>
-          </span>
-        </span>
-      </button>
-    </div>
-  );
-};
+    return (
+        <div id="appearence" className={styles.content}>
+            <button type="button" id="theme">
+                <span className={styles.check}>
+                    <span className={styles.circle}>
+                        <span className={styles.iconInactive}>
+                            <Sun size={12} />
+                        </span>
+                        <span className={styles.icon}>
+                            <Moon size={12} />
+                        </span>
+                    </span>
+                </span>
+            </button>
+        </div>
+    )
+}
 
-export default ThemeToggle;
+export default ThemeToggle
