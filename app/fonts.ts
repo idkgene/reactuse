bf42759dbb60db04a1c119dcd3deefec33adbf6e
Lystrillian <webeugene@proton.me>
2024-05-11 12:10:07 +0300

    removed unused import in fonts ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,41 +1,40 @@
-import localFont from "next/font/local";
-import Roboto from "next/font/google";
+import localFont from 'next/font/local'
 
 const WixMadeforDisplayExtraBold = localFont({
-  src: "../public/fonts/WixMadeforDisplay-ExtraBold.ttf",
-  fallback: ["system-ui", "Roboto"],
-});
+    src: '../public/fonts/WixMadeforDisplay-ExtraBold.ttf',
+    fallback: ['system-ui', 'Roboto'],
+})
 
 const WixMadeforDisplayBold = localFont({
-  src: "../public/fonts/WixMadeforDisplay-Bold.ttf",
-  fallback: ["system-ui", "Roboto"],
-});
+    src: '../public/fonts/WixMadeforDisplay-Bold.ttf',
+    fallback: ['system-ui', 'Roboto'],
+})
 
 const WixMadeforDisplaySemiBold = localFont({
-  src: "../public/fonts/WixMadeforDisplay-SemiBold.ttf",
-  fallback: ["system-ui", "Roboto"],
-});
+    src: '../public/fonts/WixMadeforDisplay-SemiBold.ttf',
+    fallback: ['system-ui', 'Roboto'],
+})
 
 const WixMadeforDisplayMedium = localFont({
-  src: "../public/fonts/WixMadeforDisplay-Medium.ttf",
-  fallback: ["system-ui", "Roboto"],
-});
+    src: '../public/fonts/WixMadeforDisplay-Medium.ttf',
+    fallback: ['system-ui', 'Roboto'],
+})
 
 const WixMadeforDisplayRegular = localFont({
-  src: "../public/fonts/WixMadeforDisplay-Regular.ttf",
-  fallback: ["system-ui", "Roboto"],
-});
+    src: '../public/fonts/WixMadeforDisplay-Regular.ttf',
+    fallback: ['system-ui', 'Roboto'],
+})
 
 const WixMadeForDisplayVariable = localFont({
-  src: "../public/fonts/WixMadeforDisplay-VariableFont_wght.ttf",
-  fallback: ["system-ui", "Roboto"],
-});
+    src: '../public/fonts/WixMadeforDisplay-VariableFont_wght.ttf',
+    fallback: ['system-ui', 'Roboto'],
+})
 
 export {
-  WixMadeforDisplayExtraBold,
-  WixMadeforDisplayBold,
-  WixMadeforDisplaySemiBold,
-  WixMadeforDisplayMedium,
-  WixMadeforDisplayRegular,
-  WixMadeForDisplayVariable,
-};
+    WixMadeforDisplayExtraBold,
+    WixMadeforDisplayBold,
+    WixMadeforDisplaySemiBold,
+    WixMadeforDisplayMedium,
+    WixMadeforDisplayRegular,
+    WixMadeForDisplayVariable,
+}
