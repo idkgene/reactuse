38c79a2f3845143fbec1d75a3a4af7d73270abf4
Lystrillian <webeugene@proton.me>
2024-05-15 00:36:46 +0300

    📌 isDefined util added

--- hooks/@Utilities/isDefined.ts
+++ hooks/@Utilities/isDefined.ts
@@ -1,3 +1,10 @@
-export function isDefined<T>(v: T): v is Exclude<T, null | undefined> {
-  return v !== null && v !== undefined
+/**
+ * Non-nullish checking type guard for Ref.
+ *
+ * @param v - The value to check.
+ * @returns {boolean}
+ */
+
+export function isDefined<T>(value: T): value is Exclude<T, null | undefined> {
+  return value !== null && value !== undefined
 }
