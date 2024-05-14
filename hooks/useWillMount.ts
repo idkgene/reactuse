7475eb0028823184207040905eb145a7249ae43d
Lystrillian <webeugene@proton.me>
2024-05-15 00:36:32 +0300

    📋 useWillMount TSdoc

--- hooks/useWillMount.ts
+++ hooks/useWillMount.ts
@@ -1,5 +1,13 @@
 import { useRef } from 'react'
 
+/**
+ * The `useWillMount` custom hook in TypeScript allows you to run a callback function only once when a
+ * component is mounted.
+ * @param callback - The `callback` parameter in the `useWillMount` custom hook is a function that will
+ * be executed only once when the component is about to mount. It is a function that you pass to the
+ * `useWillMount` hook, and it will be called when the component is about to mount for
+ */
+
 export const useWillMount = (callback: () => void): void => {
   const willMountRef = useRef(true)
 
