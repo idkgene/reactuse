b6e8420c46e4200c29d0779a289988c764b984df
Lystrillian <webeugene@proton.me>
2024-05-24 23:36:58 +0300

    useSupported jsdoced

--- hooks/@Utilities/useSupported.ts
+++ hooks/@Utilities/useSupported.ts
@@ -1,16 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
+/**
+ * A custom hook to check if a feature is supported based on the provided callback.
+ *
+ * @param {() => unknown} callback - A function that check for feature support and return a truthy value if supported.
+ * @returns {boolean} `true` if the feature is supporter, otherwise `false`.
+ *
+ * @example
+ * // Usage example:
+ * const isClipboardSupported = useSupported(() => !!navigator.clipboard);
+ *
+ * if (isClipboardSupported) {
+ *   console.log('Clipboard API is supported!');
+ * } else {
+ *   console.log('Clipboard API is not supported.');
+ * }
+ */
 export function useSupported(callback: () => unknown): boolean {
-  const [isSupported, setIsSupported] = useState<boolean>(false);
+  const [isSupported, setIsSupported] = useState<boolean>(false)
 
   useEffect(() => {
     try {
-      const result = callback();
-      setIsSupported(Boolean(result));
+      const result = callback()
+      setIsSupported(Boolean(result))
     } catch {
-      setIsSupported(false);
+      setIsSupported(false)
     }
-  }, [callback]);
+  }, [callback])
 
-  return isSupported;
+  return isSupported
 }
