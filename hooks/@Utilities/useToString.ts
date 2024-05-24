2ae532c45a110f79ea9d86e464b2a138367228b6
Lystrillian <webeugene@proton.me>
2024-05-24 23:57:56 +0300

    useToString jsdoced

--- hooks/@Utilities/useToString.ts
+++ hooks/@Utilities/useToString.ts
@@ -1,10 +1,19 @@
 import { useMemo } from 'react'
 
 /**
- * Reactively convert a value or a getter function to a string.
+ * A custom hook to convert a value or a function returning a value to a string.
  *
- * @param value - The value or getter function to convert to a string.
- * @returns A memoized string representation of the value.
+ * @template T - The type of the value to be converted to a string.
+ * @param {T | (() => T)} value - The value or a function returning the value to convert to a string.
+ * @returns {string} The string representation of the resolved value.
+ *
+ * @example
+ * // Usage example:
+ * const stringValue = useToString(42);
+ * console.log(stringValue); // Output: '42'
+ *
+ * const dynamicStringValue = useToString(() => new Date().getFullYear());
+ * console.log(dynamicStringValue); // Output: '2024' (or the current year)
  */
 export function useToString<T>(value: T | (() => T)): string {
   return useMemo(() => {
