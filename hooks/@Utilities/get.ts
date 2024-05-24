e0e626e3de8e195da1569f34da1ba5541a80dfcd
Lystrillian <webeugene@proton.me>
2024-05-24 23:29:51 +0300

    get utility jsdoced

--- hooks/@Utilities/get.ts
+++ hooks/@Utilities/get.ts
@@ -1,21 +1,33 @@
-import {  MutableRefObject } from 'react'
+import { MutableRefObject } from 'react'
 
 /**
- * Shorthand for accessing the current value of a ref.
+ * Retrieves the current value stored in a gived mutable reference.
  *
- * @param ref - The ref object to access the value from.
- * @returns The current value of the ref.
+ * @param {MutableRefObject<T>} ref - The mutable reference object holding the value.
+ * @returns {T} The current value of the reference
+ *
+ * @example
+ * // Usage example:
+ * const myRef = useRef<number>(0);
+ * const value = getValue(myRef); // value will be 0
  */
 export function getValue<T>(ref: MutableRefObject<T>): T {
   return ref.current
 }
 
 /**
- * Shorthand for accessing a specific property of the current value of a ref.
+ * Retrieves specific property from the current value stored ina given mutable reference.
  *
- * @param ref - The ref object to access the value from.
- * @param key - The key of the property to access.
- * @returns The value of the specified property from the current ref value.
+ * @template T - The type of the value held by the reference.
+ * @template K - The type of the key of the property to retrieve.
+ * @param {MutableRefObject<T>} ref - The mutable reference object holding the value.
+ * @param {K} key - The key of the property to retrieve.
+ * @returns {T[K]} - The value of the specified property.
+ * @example
+ * // Usage example:
+ * type Person = { name: string; age: number; };
+ * const personRef = useRef<Person>({ name: 'Alex', age: 20 });
+ * const name = getProperty(personRef, 'name');  // name will be 'Alex'
  */
 export function getProperty<T, K extends keyof T>(
   ref: MutableRefObject<T>,
