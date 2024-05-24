7e7f0df28c69619551011a7b3491cb08e213a83c
Lystrillian <webeugene@proton.me>
2024-05-24 23:30:40 +0300

    isDefined jsdoced

--- hooks/@Utilities/isDefined.ts
+++ hooks/@Utilities/isDefined.ts
@@ -1,10 +1,21 @@
 /**
- * Non-nullish checking type guard for Ref.
+ * Checks if the given value is neither `null` nor `undefined`.
  *
- * @param v - The value to check.
- * @returns {boolean}
+ * @template T - The type of the value being checked.
+ * @param {T} value - The value to check.
+ * @returns {value is Exclude<T, null | undefined>} `true` if the value is defined, otherwise `false`.
+ *
+ * @example
+ * // Usage example:
+ * const maybeValue: string | null | undefined = 'Hello';
+ * if (isDefined(maybeValue)) {
+ *   // maybeValue is now narrowed to `string`
+ *   console.log(maybeValue);  // Output: 'Hello'
+ * }
+ *
+ * const anotherValue: number | null = null;
+ * const isAnotherDefined = isDefined(anotherValue); // Output: false
  */
-
 export function isDefined<T>(value: T): value is Exclude<T, null | undefined> {
   return value !== null && value !== undefined
 }
