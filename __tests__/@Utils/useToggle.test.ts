dd16bdf439798210fb42df35e77d49884ee1f52f
Lystrillian <webeugene@proton.me>
2024-05-25 12:14:34 +0300

    useDestructable testes

--- __tests__/@Utils/useToggle.test.ts
+++ __tests__/@Utils/useToggle.test.ts
@@ -1,58 +1,60 @@
-import { renderHook, act } from '@testing-library/react';
-import useToggle from '@hooks/@Utilities/useToggle';
+import { renderHook, act } from '@testing-library/react'
+import { useToggle } from '@hooks/@Utilities/useToggle'
 
 describe('useToggle', () => {
   it('should initialize with the default value', () => {
-    const { result } = renderHook(() => useToggle());
-    expect(result.current[0]).toBe(false);
-  });
+    const { result } = renderHook(() => useToggle())
+    expect(result.current[0]).toBe(false)
+  })
 
   it('should initialize with the provided initial value', () => {
-    const { result } = renderHook(() => useToggle(true));
-    expect(result.current[0]).toBe(true);
-  });
+    const { result } = renderHook(() => useToggle(true))
+    expect(result.current[0]).toBe(true)
+  })
 
   it('should toggle the state when toggle function is called', () => {
-    const { result } = renderHook(() => useToggle(false));
+    const { result } = renderHook(() => useToggle(false))
 
     act(() => {
-      result.current[1]();
-    });
-    expect(result.current[0]).toBe(true);
+      result.current[1]()
+    })
+    expect(result.current[0]).toBe(true)
 
     act(() => {
-      result.current[1]();
-    });
-    expect(result.current[0]).toBe(false);
-  });
+      result.current[1]()
+    })
+    expect(result.current[0]).toBe(false)
+  })
 
   it('should set the state to the provided value when toggle function is called with a value', () => {
-    const { result } = renderHook(() => useToggle(false));
+    const { result } = renderHook(() => useToggle(false))
 
     act(() => {
-      result.current[1](true);
-    });
-    expect(result.current[0]).toBe(true);
+      result.current[1](true)
+    })
+    expect(result.current[0]).toBe(true)
 
     act(() => {
-      result.current[1](false);
-    });
-    expect(result.current[0]).toBe(false);
-  });
+      result.current[1](false)
+    })
+    expect(result.current[0]).toBe(false)
+  })
 
   it('should use the custom truthy and falsy values when provided', () => {
-    const { result } = renderHook(() => useToggle('off', { truthyValue: 'on', falsyValue: 'off' }));
+    const { result } = renderHook(() =>
+      useToggle('off', { truthyValue: 'on', falsyValue: 'off' })
+    )
 
-    expect(result.current[0]).toBe('off');
-
-    act(() => {
-      result.current[1]();
-    });
-    expect(result.current[0]).toBe('on');
+    expect(result.current[0]).toBe('off')
 
     act(() => {
-      result.current[1]();
-    });
-    expect(result.current[0]).toBe('off');
-  });
-});
+      result.current[1]()
+    })
+    expect(result.current[0]).toBe('on')
+
+    act(() => {
+      result.current[1]()
+    })
+    expect(result.current[0]).toBe('off')
+  })
+})
