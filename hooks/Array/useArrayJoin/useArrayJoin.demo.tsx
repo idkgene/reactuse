a430d77d9eae864dcd2e0d281704c682fd042de7
Lystrillian <webeugene@proton.me>
2024-05-26 17:29:18 +0300

    added useArrayJoin hook and demo

---
+++ hooks/Array/useArrayJoin/useArrayJoin.demo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useArrayJoin } from './useArrayJoin'
+
+const useArrayJoinDemo = () => {
+  const list = [1, 2, 3, 4, 5]
+  const joinedList = useArrayJoin(list)
+
+  return (
+    <div>
+      <p>Original array: {list.join(', ')}</p>
+      <p>Joined array: {joinedList}</p>
+    </div>
+  )
+}
+
+export default useArrayJoinDemo
---
+++ hooks/Array/useArrayJoin/useArrayJoin.ts
@@ -0,0 +1,15 @@
+import { useMemo } from 'react'
+
+/**
+ * Reactive `Array.join` hook for React.
+ *
+ * @param list - The array to join.
+ * @param separator - A string to separate each pair of adjacent elements of the array. If omitted, the array elements are separated with a comma (",").
+ *
+ * @returns A string with all array elements joined. If list.length is 0, an empty string is returned.
+ */
+function useArrayJoin<T>(list: T[], separator?: string): string {
+  return useMemo(() => list.join(separator), [list, separator])
+}
+
+export default useArrayJoin
