a0e528cfbe0e47182e601305c166498dd2f083fd
Lystrillian <webeugene@proton.me>
2024-05-26 17:45:42 +0300

    useDefault described

---
+++ hooks/State/useDefault/useDefault.ts
@@ -0,0 +1,14 @@
+  import { useState, useCallback } from 'react'
+
+  export const useDefault = <T>(defaultValue: T | null) => {
+    const [state, setState] = useState<T | null>(() => defaultValue)
+
+    const setValue = useCallback(
+      (newValue: T | null) => {
+        setState(newValue !== null ? newValue : defaultValue)
+      },
+      [defaultValue]
+    )
+
+    return [state, setValue] as const
+  }
