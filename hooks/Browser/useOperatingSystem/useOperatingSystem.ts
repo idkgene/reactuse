37a1067f3b676101e8e10655fa7c0a626c9486b2
Lystrillian <webeugene@proton.me>
2024-05-29 10:05:17 +0300

    [feat]: useOperatingSystem introduced

---
+++ hooks/Browser/useOperatingSystem/useOperatingSystem.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+export type OperatingSystem =
+  | 'undetermined'
+  | 'macos'
+  | 'ios'
+  | 'windows'
+  | 'android'
+  | 'linux'
+
+const getOperatingSystem = (userAgent: string): OperatingSystem => {
+  if (/(Macintosh)|(MacIntel)|(MacPPC)|(Mac68K)/i.test(userAgent))
+    return 'macos'
+  if (/(iPhone)|(iPad)|(iPod)/i.test(userAgent)) return 'ios'
+  if (/(Win32)|(Win64)|(Windows)|(WinCE)/i.test(userAgent)) return 'windows'
+  if (/Android/i.test(userAgent)) return 'android'
+  if (/Linux/i.test(userAgent)) return 'linux'
+  return 'undetermined'
+}
+
+/**
+ * @name useOperatingSystem
+ * @description - Hook that returns the operating system of the current browser
+ *
+ * @returns {OperatingSystem} The operating system
+ *
+ * @example
+ * const operatingSystem = useOperatingSystem();
+ */
+export const useOperatingSystem = (): OperatingSystem => {
+  const [operatingSystem, setOperatingSystem] =
+    useState<OperatingSystem>('undetermined')
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const { userAgent } = window.navigator
+      setOperatingSystem(getOperatingSystem(userAgent))
+    }
+  }, [])
+
+  return operatingSystem
+}
