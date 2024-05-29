a873191e9a256b5123e84abf2ea58437840e0c09
Lystrillian <webeugene@proton.me>
2024-05-29 19:32:33 +0300

    [chore]: added demo for useIsomorphicLayout effect

---
+++ hooks/Component/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.demo.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect'
+
+const UseIsomorphicLayoutEffectDemo = () => {
+  const [count, setCount] = React.useState(0)
+
+  useIsomorphicLayoutEffect(() => {
+    console.log('Component mounted or updated')
+  })
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
+export default UseIsomorphicLayoutEffectDemo
