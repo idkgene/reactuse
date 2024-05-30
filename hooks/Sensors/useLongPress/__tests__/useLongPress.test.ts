be61fbd86028ec001953aa663e92ed4f48b99067
Lystrillian <webeugene@proton.me>
2024-05-30 11:47:23 +0300

    [chore]: `useLongPress` demo craeted & moved tests to a separate folder

--- hooks/Sensors/useLongPress/__tests__/useLongPress.test.ts
+++ hooks/Sensors/useLongPress/__tests__/useLongPress.test.ts
@@ -1,5 +1,5 @@
 import { renderHook, act } from '@testing-library/react'
-import { useLongPress } from './useLongPress'
+import { useLongPress } from '../useLongPress'
 
 describe('useLongPress', () => {
   jest.useFakeTimers()
