03e43e63336c5d506a54d1c38cf73a64c1360631
Lystrillian <webeugene@proton.me>
2024-05-31 03:34:41 +0300

    [chore]: useWillUnmount organized

--- hooks/Component/useWillUnmount/useWillUnmount.ts
+++ hooks/Component/useWillUnmount/useWillUnmount.ts
@@ -1,5 +1,12 @@
 import { useEffect } from 'react'
 
+/**
+ * @name useWillUnmount
+ * @description A custom React hook that allows you to run a callback function when a component unmounts.
+ * 
+ * @param callback - The callback function to be executed when the component unmounts.
+ * @returns {void}
+ */
 export const useWillUnmount = (callback: () => void) => {
   useEffect(() => callback, [])
 }
