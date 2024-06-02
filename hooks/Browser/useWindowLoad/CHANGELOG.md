de0c5eebcb77b3c103e8decb425da5d819e57d1b
Lystrillian <webeugene@proton.me>
2024-06-02 20:32:29 +0300

    [chore]: useWindowLoad organized

---
+++ hooks/Browser/useWindowLoad/CHANGELOG.md
@@ -0,0 +1,7 @@
+# useWindowLoad
+
+## 0.0.1
+
+### Patch Changes
+
+- [``]()- Introduced useWindowLoad
---
+++ hooks/Browser/useWindowLoad/useWindowLoad.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+type WindowLoadState = 'LOADING' | 'LOADED'
+
+/**
+ * Hook that returns the current state of the `window's` loading proccess.
+ *
+ * @returns {WindowLoadState} The current state of the window's loading process. It can be either 'LOADING' or 'LOADED'.
+ *
+ * @example
+ * const windowLoadState = useWindowLoad();
+ * console.log(windowLoadState); // Output: 'LOADING' or 'LOADED'
+ */
+export const useWindowLoad = (): WindowLoadState => {
+  const [loadState, setLoadState] = useState<WindowLoadState>('LOADING')
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoadState('LOADED')
+    }
+
+    if (typeof window !== 'undefined') {
+      if (document.readyState === 'complete') {
+        setLoadState('LOADED')
+      } else {
+        window.addEventListener('load', handleLoad)
+      }
+
+      return () => {
+        window.removeEventListener('load', handleLoad)
+      }
+    }
+  }, [])
+
+  return loadState
+}
