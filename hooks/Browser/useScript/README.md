44d96603805b2ef2a98b1c26d50d48293ad02307
Lystrillian <webeugene@proton.me>
2024-06-02 20:45:08 +0300

    [chore]: useScript organized

---
+++ hooks/Browser/useScript/README.md
@@ -0,0 +1,19 @@
+# useScript
+
+Dynamically loads a script from a given URL.
+
+Please refer to the [documentation](#) for more information.
+
+## Installation
+
+```bash
+npx changeelog/reactuse@latest add useScript
+```
+
+## Contribution
+
+Yes please! See the [contributing guidelines](#) for more details!
+
+## License
+
+This project is licensed under the terms of the [MIT License](/LICENSE)
--- hooks/Browser/useScript/useScript.ts
+++ hooks/Browser/useScript/useScript.ts
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
+import { useEffect } from 'react'
 
 /**
+ * Dynamically loads a from a givent URL.
+ *
  * @param {string} url - The URL of the script to be loaded.
+ * @return {void} This function does not return anything
+ *
+ * @example
+ * const scriptUrl = 'https://example.com/script.js'
+ * useScript(scriptUrl)
  */
 
 export function useScript(url: string): void {
