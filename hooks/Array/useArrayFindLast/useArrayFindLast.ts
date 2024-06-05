6ef01194c08330b9982af45f2748144b43619f64
Lystrillian <webeugene@proton.me>
2024-06-05 15:53:41 +0300

    [chore]: useArrayFindLast jsdoced

--- hooks/Array/useArrayFindLast/useArrayFindLast.ts
+++ hooks/Array/useArrayFindLast/useArrayFindLast.ts
@@ -1,24 +1,34 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo } from 'react';
 
 /**
- * @name useArrayFindLast
- * @description `Array.findLast`
+ * `Array.findLast`
  *
- * @param list - the array to search.
- * @param fn - a function to test each element.
+ * Provides a memoized version of the `Array.findLast` method.
  *
- * @returns the last element in the array that satisfies the provided testing function. Otherwise, undefined is returned.
+ * @param {T[]} list - The array to search.
+ * @param {(element: T, index: number, array: T[]) => boolean} fn - A function to test each element in the array.
+ * @returns {T | undefined} The last element in the array that satisfies the provided testing function. Otherwise, `undefined` is returned.
+ *
+ * @example
+ * const users = [
+ *   { id: 1, name: 'Alice' },
+ *   { id: 2, name: 'Bob' },
+ *   { id: 3, name: 'Charlie' }
+ * ];
+ * const findLastUserByName = (user) => user.name.startsWith('B');
+ * const lastUser = useArrayFindLast(users, findLastUserByName);
+ * console.log(lastUser); // Output: { id: 2, name: 'Bob' }
  */
 export function useArrayFindLast<T>(
   list: T[],
   fn: (element: T, index: number, array: T[]) => boolean
 ): T | undefined {
-  const memoizedFn = useCallback(fn, [fn])
+  const memoizedFn = useCallback(fn, [fn]);
 
   const findLast = useCallback(
     (array: T[]) => array.slice().reverse().find(memoizedFn),
     [memoizedFn]
-  )
+  );
 
-  return useMemo(() => findLast(list), [findLast, list])
+  return useMemo(() => findLast(list), [findLast, list]);
 }
