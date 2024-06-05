fb0a44e40cf341eb99892a5870eb16c5d17ece8c
Lystrillian <webeugene@proton.me>
2024-06-05 16:00:45 +0300

    [chore]: `useArraySome` jsdoc update

--- hooks/Array/useArraySome/useArraySome.ts
+++ hooks/Array/useArraySome/useArraySome.ts
@@ -1,17 +1,27 @@
-import { useMemo } from 'react'
+import { useMemo } from 'react';
 
 /**
- * @name useArraySome
- * @description `Array.some` hook for React.
+ * `Array.some` hook for React.
  *
- * @param list - The array to check.
- * @param predicate - A function to test each element.
+ * Provides a memoized version of the `Array.some` method that tests whether at least one element in the array passes the provided predicate function.
  *
- * @returns `true` if the `predicate` function returns a truthy value for any element from the array. Otherwise, `false`.
+ * @param {T[]} list - The array to check.
+ * @param {(element: T, index: number, array: T[]) => unknown} predicate - A function to test each element in the array.
+ * @returns {boolean} `true` if the `predicate` function returns a truthy value for any element in the array, otherwise `false`.
+ *
+ * @example
+ * const numbers = [1, 2, 3, 4, 5];
+ * const hasEvenNumber = useArraySome(numbers, (number) => number % 2 === 0);
+ * console.log(hasEvenNumber); // Output: true
+ *
+ * @example
+ * const users = [{ isAdmin: false }, { isAdmin: true }];
+ * const hasAdmin = useArraySome(users, (user) => user.isAdmin);
+ * console.log(hasAdmin); // Output: true
  */
 export function useArraySome<T>(
   list: T[],
   predicate: (element: T, index: number, array: T[]) => unknown
 ): boolean {
-  return useMemo(() => list.some(predicate), [list, predicate])
+  return useMemo(() => list.some(predicate), [list, predicate]);
 }
