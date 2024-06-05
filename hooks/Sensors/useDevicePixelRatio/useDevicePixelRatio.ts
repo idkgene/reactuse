c23095016b499a7d56637dc2675c1c4a52bfa53d
Lystrillian <webeugene@proton.me>
2024-06-05 22:35:22 +0300

    [chore]: useDevicePixelRatio jsdoc

--- hooks/Sensors/useDevicePixelRatio/useDevicePixelRatio.ts
+++ hooks/Sensors/useDevicePixelRatio/useDevicePixelRatio.ts
@@ -1,13 +1,46 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Options for the useDevicePixelRatio hook.
+ *
+ * @typedef {Object} UseDevicePixelRatioOptions
+ * @property {Window} [window] - The window object to use instead of the
+ * default window object.
+ */
 interface UseDevicePixelRatioOptions {
   window?: Window;
 }
 
+/**
+ * Return value of the useDevicePixelRatio hook.
+ *
+ * @typedef {Object} UseDevicePixelRatioReturn
+ * @property {number} pixelRatio - The current device pixel ratio.
+ */
 interface UseDevicePixelRatioReturn {
   pixelRatio: number;
 }
 
+/**
+ * Returns the device pixel ratio of the curent window.
+ *
+ * The device pixel ratio is a ratio of the resolution in physical pixels
+ * to the resolution in CSS pixels for the current window. This hook
+ * listens for changes in the device pixel ratio and updates the
+ * returned value accordingly.
+ *
+ * @param {UseDevicePixelRatioOptions} options - Optional configuration
+ * object.
+ * @param {Window} options.window - The window object to use instead of the
+ * default window object.
+ *
+ * @returns {UseDevicePixelRatioReturn} An object containing the current
+ * device pixel ratio.
+ *
+ * @example
+ * // Example usage of the hook
+ * const { pixelRatio } = useDevicePixelRatio();
+ */
 export function useDevicePixelRatio(
   options: UseDevicePixelRatioOptions = {}
 ): UseDevicePixelRatioReturn {
