2f92e23e1ced8eeaf645521d2bf6e5fd1539eb80
Lystrillian <webeugene@proton.me>
2024-06-05 15:58:27 +0300

    [chore]: `useArrayMap` jsdoc updated

--- hooks/Array/useArrayMap/useArrayMap.ts
+++ hooks/Array/useArrayMap/useArrayMap.ts
@@ -1,17 +1,27 @@
-import { useMemo } from 'react'
+import { useMemo } from 'react';
 
 /**
- * @name useArrayMap
- * @description`Array.map` hook for React.
+ * `Array.map` hook for React.
  *
- * @param list - The array to map over.
- * @param callback - A function that is called for every element of the given `list`. Each time `callback` executes, the returned value is added to the new array.
+ * Provides a memoized version of the `Array.map` method that creates a new array populated with the results of calling a provided function on every element in the given array.
  *
- * @returns A new array with each element being the result of the callback function.
+ * @param {T[]} list - The array to map over.
+ * @param {(element: T, index: number, array: T[]) => U} callback - A function that is called for every element of the given `list`. Each time `callback` executes, the returned value is added to the new array.
+ * @returns {U[]} A new array with each element being the result of the callback function.
+ *
+ * @example
+ * const numbers = [1, 2, 3, 4];
+ * const squaredNumbers = useArrayMap(numbers, (number) => number * number);
+ * console.log(squaredNumbers); // Output: [1, 4, 9, 16]
+ *
+ * @example
+ * const users = [{ name: 'Alice' }, { name: 'Bob' }];
+ * const userNames = useArrayMap(users, (user) => user.name);
+ * console.log(userNames); // Output: ['Alice', 'Bob']
  */
 export function useArrayMap<T, U = T>(
   list: T[],
   callback: (element: T, index: number, array: T[]) => U
 ): U[] {
-  return useMemo(() => list.map(callback), [list, callback])
+  return useMemo(() => list.map(callback), [list, callback]);
 }
