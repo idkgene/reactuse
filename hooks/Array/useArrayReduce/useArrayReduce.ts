a52f423ca0e8a3d165941d2f0142416c8efb3d62
Lystrillian <webeugene@proton.me>
2024-06-05 15:58:51 +0300

    [chore]: `useArrayReduce` jsdoc update

--- hooks/Array/useArrayReduce/useArrayReduce.ts
+++ hooks/Array/useArrayReduce/useArrayReduce.ts
@@ -1,20 +1,30 @@
-import { useMemo } from 'react'
+import { useMemo } from 'react';
 
 type UseArrayReducer<PV, CV, R> = (
   previousValue: PV,
   currentValue: CV,
   currentIndex: number
-) => R
+) => R;
 
 /**
- * @name useArrayReduce
- * @description `Array.reduce` hook for React.
+ * `Array.reduce` hook for React.
  *
- * @param list - The array to reduce.
- * @param reducer - A "reducer" function.
- * @param initialValue - A value to be initialized the first time when the callback is called.
+ * Provides a memoized version of the `Array.reduce` method that applies a reducer function to each element of the array, resulting in a single output value.
  *
- * @returns The value that results from running the "reducer" callback function to completion over the entire array.
+ * @param {T[]} list - The array to reduce.
+ * @param {UseArrayReducer<U, T, U>} reducer - A "reducer" function that accumulates the array elements.
+ * @param {U} initialValue - A value to initialize the accumulator the first time the callback is called.
+ * @returns {U} The value that results from running the "reducer" callback function to completion over the entire array.
+ *
+ * @example
+ * const numbers = [1, 2, 3, 4];
+ * const sum = useArrayReduce(numbers, (accumulator, number) => accumulator + number, 0);
+ * console.log(sum); // Output: 10
+ *
+ * @example
+ * const objects = [{ count: 1 }, { count: 2 }];
+ * const totalCount = useArrayReduce(objects, (accumulator, obj) => accumulator + obj.count, 0);
+ * console.log(totalCount); // Output: 3
  */
 export function useArrayReduce<T, U>(
   list: T[],
@@ -24,5 +34,5 @@ export function useArrayReduce<T, U>(
   return useMemo(
     () => list.reduce(reducer, initialValue),
     [list, reducer, initialValue]
-  )
+  );
 }
