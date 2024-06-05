35e5e6c34af282bc0a68ddec244cde2dd8523cf1
Lystrillian <webeugene@proton.me>
2024-06-05 15:58:19 +0300

    [chore]: `useArrayJoin` jsdoc updated

--- hooks/Array/useArrayJoin/useArrayJoin.ts
+++ hooks/Array/useArrayJoin/useArrayJoin.ts
@@ -1,16 +1,26 @@
-import { useMemo } from 'react'
+import { useMemo } from 'react';
 
 /**
- * @name useArrayJoin
- * @description `Array.join` hook for React.
+ * `Array.join` hook for React.
  *
- * @param list - The array to join.
- * @param separator - A string to separate each pair of adjacent elements of the array. If omitted, the array elements are separated with a comma (",").
+ * Provides a memoized version of the `Array.join` method that joins all elements of an array into a string separated by the specified separator.
  *
- * @returns A string with all array elements joined. If list.length is 0, an empty string is returned.
+ * @param {T[]} list - The array to join.
+ * @param {string} [separator] - A string to separate each pair of adjacent elements of the array. If omitted, the array elements are separated by a comma (",").
+ * @returns {string} A string with all array elements joined. If `list.length` is `0`, an empty string is returned.
+ *
+ * @example
+ * const words = ['hello', 'world'];
+ * const joinedWords = useArrayJoin(words, ' ');
+ * console.log(joinedWords); // Output: 'hello world'
+ *
+ * @example
+ * const numbers = [1, 2, 3];
+ * const joinedNumbers = useArrayJoin(numbers, '-');
+ * console.log(joinedNumbers); // Output: '1-2-3'
  */
 function useArrayJoin<T>(list: T[], separator?: string): string {
-  return useMemo(() => list.join(separator), [list, separator])
+  return useMemo(() => list.join(separator), [list, separator]);
 }
 
-export default useArrayJoin
+export default useArrayJoin;
