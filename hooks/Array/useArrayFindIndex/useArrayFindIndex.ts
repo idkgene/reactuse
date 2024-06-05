85e0f791f9b23566cb10a4f0578bf121eb1ee47d
Lystrillian <webeugene@proton.me>
2024-06-05 15:52:29 +0300

    [chore]: `useArrayFindIndex` jsdoc update

--- hooks/Array/useArrayFindIndex/useArrayFindIndex.ts
+++ hooks/Array/useArrayFindIndex/useArrayFindIndex.ts
@@ -2,13 +2,23 @@ import { useMemo } from 'react'
 import { UseArrayEveryPredicate } from '../array'
 
 /**
- * @name useArrayFindIndex
- * @description `Array.findIndex` hook for React.
+ * `Array.findIndex` hook for React.
  *
- * @param list - The array to search.
- * @param predicate - A function to test each element.
+ * Provides a memoized version of the `Array.findIndex` method.
  *
- * @returns The index of the first element in the array that passes the test. Otherwise, "-1".
+ * @param {T[]} list - The array to search.
+ * @param {UseArrayEveryPredicate<T>} predicate - A function to test each element in the array.
+ *
+ * @returns {number} The index of the first element in the array that passes the test. Otherwise, `-1`.
+ *
+ * @example
+ * const users = [
+ *   { id: 1, name: 'Alice' },
+ *   { id: 2, name: 'Bob' }
+ * ];
+ * const findUserIndexById = (user) => user.id === 2;
+ * const userIndex = useArrayFindIndex(users, findUserIndexById);
+ * console.log(userIndex); // Output: 1
  */
 export function useArrayFindIndex<T>(
   list: T[],
