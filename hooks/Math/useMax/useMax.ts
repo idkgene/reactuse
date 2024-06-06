67ad9fe8fde5a6ac22a8b55e444d7dd8219427cb
Lystrillian <webeugene@proton.me>
2024-06-06 04:47:46 +0300

    [feat]: `useMin` and `useMax` renewed and organized

--- hooks/Math/useMax/useMax.ts
+++ hooks/Math/useMax/useMax.ts
@@ -1,17 +1,32 @@
-import { useCallback, useEffect, useState } from "react";
+import { useMemo } from 'react';
 
-type UpdateMaxFn = (newNumbers: number[]) => void;
+type ValueOrFunction<T> = T | (() => T);
 
-export const useMax = (numbers: number[]): [number, UpdateMaxFn] => {
-  const [max, setMax] = useState<number>(() => Math.max(...numbers));
+function resolveValue<T>(value: ValueOrFunction<T>): T {
+  return typeof value === 'function' ? (value as Function)() : value;
+}
 
-  const updateMax: UpdateMaxFn = useCallback((newNumbers: number[]) => {
-    setMax(Math.max(...newNumbers));
-  }, []);
-
-  useEffect(() => {
-    updateMax(numbers);
-  }, [numbers, updateMax]);
-
-  return [max, updateMax];
-};
+/**
+ * Returns the maximum value from the given arguments or array of values.
+ *
+ * @param {...any[]} args - The values or arrays of values to compare.
+ * @returns {number} The maximum value among the given arguments or array of values.
+ *
+ * @example
+ * const maxValue = useMax(1, 2, 3);
+ * // maxValue is 3
+ *
+ * @example
+ * const maxValue = useMax([1, 2, 3]);
+ * // maxValue is 3
+ */
+export function useMax(...args: any[]): number {
+  return useMemo(() => {
+    if (args.length === 1 && Array.isArray(resolveValue(args[0]))) {
+      const array = resolveValue(args[0]) as ValueOrFunction<number>[];
+      return Math.max(...array.map(resolveValue));
+    } else {
+      return Math.max(...args.map(resolveValue));
+    }
+  }, args);
+}
--- hooks/Math/useMin/useMin.ts
+++ hooks/Math/useMin/useMin.ts
@@ -1,19 +1,33 @@
-import { useEffect, useRef, useState } from 'react'
+import { useMemo } from 'react';
 
-type UpdateMinFn = (newNumbers: number[]) => void
+type ValueOrFunction<T> = T | (() => T);
 
-export const useMin = (numbers: number[]): [number, UpdateMinFn] => {
-  const [min, setMin] = useState<number>(() => Math.min(...numbers))
-  const updateMinRef = useRef<UpdateMinFn>(() => {})
+function resolveValue<T>(value: ValueOrFunction<T>): T {
+  return typeof value === 'function' ? (value as Function)() : value;
+}
 
-  useEffect(() => {
-    const updateMin: UpdateMinFn = (newNumbers: number[]) => {
-      setMin(Math.min(...newNumbers))
+/**
+ * Returns the minimum value from the given arguments.
+ *
+ * The `useMin` hook accepts any number of arguments, which can be either
+ * individual values or arrays of values. If an array is provided, the hook
+ * will return the minimum value from the array. If individual values are
+ * provided, the hook will return the minimum value from those values.
+ *
+ * @param {...(number | (() => number) | number[])} args - The values or arrays of values from which to find the minimum.
+ * @returns {number} The minimum value from the given arguments.
+ *
+ * @example
+ * const minValue = useMin(1, 2, 3, 4, -1, 10); // Returns -1
+ * const minArrayValue = useMin([5, 10, 15, -5, 0]); // Returns -5
+ */
+export function useMin(...args: any[]): number {
+  return useMemo(() => {
+    if (args.length === 1 && Array.isArray(resolveValue(args[0]))) {
+      const array = resolveValue(args[0]) as ValueOrFunction<number>[];
+      return Math.min(...array.map(resolveValue));
+    } else {
+      return Math.min(...args.map(resolveValue));
     }
-
-    updateMin(numbers)
-    updateMinRef.current = updateMin
-  }, [numbers])
-
-  return [min, updateMinRef.current]
+  }, args);
 }
