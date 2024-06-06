7617d0c0e7f85580494746a1dd6100db686c315a
Lystrillian <webeugene@proton.me>
2024-06-06 14:19:05 +0300

    [chore]: minor demo update and linked CONTRIBUTING.md to README

--- hooks/State/createGlobalState/createGlobalState.demo.tsx
+++ hooks/State/createGlobalState/createGlobalState.demo.tsx
@@ -1,38 +1,68 @@
-import React from 'react'
-import { createGlobalState } from './createGlobalState'
+'use client';
+import React from 'react';
+import { createGlobalState } from './createGlobalState';
 
-// Create a global state
-const useGlobalCounter = createGlobalState(0)
+const useGlobalState = createGlobalState({
+  value1: 'Banana',
+  value2: 'Cherry',
+  value3: 'Apple',
+});
 
-const Counter: React.FC = () => {
-  const [count, setCount] = useGlobalCounter()
+const Demo: React.FC = () => {
+  const [state, setState] = useGlobalState();
 
-  return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-    </div>
-  )
-}
-
-const AnotherCounter: React.FC = () => {
-  const [count] = useGlobalCounter()
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState(prevState => ({ ...prevState, [name]: value }));
+  };
 
   return (
-    <div>
-      <h2>Another Counter: {count}</h2>
+    <div className="bg-[#161618] p-[2em] relative mb-[10px] rounded-[8px] transition-colors">
+      <p className="text-[rgba(235,235,245,.6)] absolute top-0 right-[10px] text-[12px] font-medium transition-colors leading-[28px] my-[.5rem]">
+        <a
+          href="#"
+          target="_blank"
+          className="font-medium text-underline underline-offset-2 transition-colors text-[#44bd87]"
+        >
+          source
+        </a>
+      </p>
+      <div>
+        <input
+          type="text"
+          name="value1"
+          className="block text-[.9rem] pt-[.5em] px-[1em] pb-[.4em] border border-solid border-[#2e2e32] rounded outline-none bg-[#1b1b1f] max-w-[20rem] my-[.5rem]"
+          value={state.value1}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="value2"
+          value={state.value2}
+          className="block text-[.9rem] pt-[.5em] px-[1em] pb-[.4em] border border-solid border-[#2e2e32] rounded outline-none bg-[#1b1b1f] max-w-[20rem] my-[.5rem]"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="value3"
+          className="block text-[.9rem] pt-[.5em] px-[1em] pb-[.4em] border border-solid border-[#2e2e32] rounded outline-none bg-[#1b1b1f] max-w-[20rem] my-[.5rem]"
+          value={state.value3}
+          onChange={handleInputChange}
+        />
+        <pre className="overflow-auto opacity-80">
+          {JSON.stringify(state, null, 2)}
+        </pre>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const CreateGlobalStateDemoPage: React.FC = () => {
   return (
-    <div>
-      <Counter />
-      <AnotherCounter />
+    <div className="p-[2em] relative mb-[10px] rounded-[8px] transition-colors">
+      <Demo />
     </div>
-  )
-}
+  );
+};
 
-export default CreateGlobalStateDemoPage
+export default CreateGlobalStateDemoPage;
--- hooks/State/useBoolean/useBoolean.demo.tsx
+++ hooks/State/useBoolean/useBoolean.demo.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import { useBoolean } from './useBoolean'
+'use client';
+import React from 'react';
+import { useBoolean } from './useBoolean';
+import Demo from '@/components/Common/Demo/demo';
 
 const UseBooleanDemo = () => {
-  const [isToggled, toggleIsToggled, setIsToggled] = useBoolean(false)
+  const [isToggled, toggleIsToggled, setIsToggled] = useBoolean(false);
 
   return (
-    <div>
-      <p>The toggle is {isToggled ? 'ON' : 'OFF'}.</p>
-      <button onClick={toggleIsToggled}>Toggle</button>
-      <button onClick={() => setIsToggled(true)}>Turn ON</button>
-      <button onClick={() => setIsToggled(false)}>Turn OFF</button>
-    </div>
-  )
-}
+    <Demo href="#">
+      <p>
+        The toggle is{' '}
+        {isToggled ? (
+          <span className="text-[#44bd87]">ON</span>
+        ) : (
+          <span className="text-[#ff0f0f]">OFF</span>
+        )}
+        .
+      </p>
+      <div className="flex flex-col">
+        <button onClick={toggleIsToggled}>Toggle</button>
+        <button onClick={() => setIsToggled(true)}>Turn ON</button>
+        <button onClick={() => setIsToggled(false)}>Turn OFF</button>
+      </div>
+    </Demo>
+    // <div>
+    // </div>
+  );
+};
 
-export default UseBooleanDemo
+export default UseBooleanDemo;
