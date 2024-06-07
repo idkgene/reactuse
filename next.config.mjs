76b0e7286dd74710b6be825103873a2af13517ad
Lystrillian <webeugene@proton.me>
2024-06-07 18:13:11 +0300

    [chore]: removed unncessary files

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,26 @@
-import createMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import remarkToc from 'remark-toc'
-import rehypeSlug from 'rehype-slug'
-import rehypeShiki from '@shikijs/rehype'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
         hostname: 'github.com',
         port: '',
+        protocol: 'https',
       },
       {
-        protocol: 'https',
         hostname: 'gitlab.com',
         port: '',
+        protocol: 'https',
       },
       {
-        protocol: 'https',
         hostname: 'bitbucket.org',
         port: '',
+        protocol: 'https',
       },
     ],
   },
-}
-
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-  options: {
-    remarkPlugins: [remarkGfm, remarkToc],
-    rehypePlugins: [rehypeSlug, rehypeShiki]
-  },
-})
+};
 
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default nextConfig;
