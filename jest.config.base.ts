2a455020a8d64742226a87c7afb46fa915104678
Lystrillian <webeugene@proton.me>
2024-06-10 20:18:31 +0300

    [chore]: removed alex and revert jest config

--- jest.config.base.ts
+++
@@ -1,7 +0,0 @@
-export const baseJestConfig = {
-  preset: 'ts-jest',
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jest-environment-jsdom',
-  // testMatch: ['<rootDir>/tests/**/*.test.(ts|tsx)'],
-};
