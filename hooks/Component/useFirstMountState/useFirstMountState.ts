9951c062b02fcdf3e249001ef7d6af9222e8cc62
Lystrillian <webeugene@proton.me>
2024-06-17 05:50:53 +0300

    [chore]: `useFirstMountState` hook jsdoced and added link to `contributing.md`

--- hooks/Component/useFirstMountState/useFirstMountState.ts
+++ hooks/Component/useFirstMountState/useFirstMountState.ts
@@ -1,11 +1,24 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 
+/**
+ * React hook that returns `true` if the component is mounting for the first time, and `false` otherwise.
+ *
+ * @returns {boolean} `true` if the component is mounting for the first time, `false` otherwise.
+ *
+ * @example
+ * const isFirstMount = useFirstMountState();
+ * if (isFirstMount) {
+ *    console.log('Component mounted for the first time')
+ * } else {
+ *    console.log('Component has been updated')
+ * }
+ */
 export function useFirstMountState(): boolean {
-  const isInitialMount = useRef<boolean>(true)
+  const isInitialMount = useRef<boolean>(true);
 
   useEffect(() => {
-    isInitialMount.current = false
-  }, []) // Empty dependency array ensures the effect runs only once
+    isInitialMount.current = false;
+  }, []);
 
-  return isInitialMount.current
+  return isInitialMount.current;
 }
