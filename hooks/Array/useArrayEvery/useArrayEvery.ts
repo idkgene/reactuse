f67ab8ae679b5692612f3867a021de4783c3e16a
Lystrillian <webeugene@proton.me>
2024-06-19 01:44:36 +0300

    [chore]: improved jsdoc for `useArrayEvery`

--- hooks/Array/useArrayEvery/useArrayEvery.ts
+++ hooks/Array/useArrayEvery/useArrayEvery.ts
@@ -3,17 +3,21 @@ import { useMemo } from 'react';
 import { UseArrayEveryPredicate } from '../array';
 
 /**
- * `A React hook that provides a memoized implementation of the `Array.every()` method.
+ * A React hook that provides a memoized implementation of the
+ * [`Array.prototype.every`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every) method.
  *
+ * @template T - The type of elements in the array.
  * @param {T[]} list - The array to check.
  * @param {UseArrayEveryPredicate<T>} predicate - A function to test each element.
  * @returns {boolean} `true` if the `predicate` function returns a truthy value for every element from the array. Otherwise, `false`.
  *
  * @example
+ * import { useArrayEvery } from './useArrayEvery';
+ *
  * const numbers = [2, 4, 6, 8];
  *
  * const areAllEven = useArrayEvery(numbers, (number) => number % 2 === 0);
- *
+ * // areAllEven will be `true`
  */
 export function useArrayEvery<T>(
   list: T[],
