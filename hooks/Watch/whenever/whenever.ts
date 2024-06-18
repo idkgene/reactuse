69c190971a1cd9daac43927da0b2683c8b59d354
Lystrillian <webeugene@proton.me>
2024-06-19 02:05:04 +0300

    [chore]: `whenever` jsdoced

--- hooks/Watch/whenever/whenever.ts
+++ hooks/Watch/whenever/whenever.ts
@@ -1,24 +1,35 @@
-import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
+
 import type { WatchCallback, WheneverOptions } from '../watch';
 
 /**
  * React utility that watches a value and triggers a callback whenever the value is truthy.
  *
  * @template T
- * @param {T | false | null | undefined} value - The value to watch. If falsy, the callback will not be triggered.
+ * @param {T | false | null | undefined} value - The value to monitor. The callback
+ * will only be triggered if this value is truthy.
  * @param {WatchCallback<T>} callback - The function to call whenever the value is truthy.
+ * Receives the current value and the previous value as arguments.
  * @param {WheneverOptions} [options] - Additional options for the hook.
+ * @param {boolean} [options.once=false] - If true, the callback will be triggered only once.
+ * @returns {void}
+ *
+ * @example
+ * // Example usage of useWhenever
+ * useWhenever(someTruthyValue, (current, previous) => {
+ *   console.log(`Value changed from ${previous} to ${current}`);
+ * }, { once: true });
  */
-export function whenever<T>(
+export function useWhenever<T>(
   value: T | false | null | undefined,
   callback: WatchCallback<T>,
   options?: WheneverOptions
-) {
+): void {
   const { once = false } = options || {};
-  const [previous, setPrevious] = React.useState<T | undefined>(undefined);
-  const hasTriggeredRef = React.useRef(false);
+  const [previous, setPrevious] = useState<T | undefined>(undefined);
+  const hasTriggeredRef = useRef(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (value) {
       if (!once || !hasTriggeredRef.current) {
         callback(value, previous);
@@ -28,3 +39,5 @@ export function whenever<T>(
     }
   }, [value, callback, previous, once]);
 }
+
+export const whenever = useWhenever
