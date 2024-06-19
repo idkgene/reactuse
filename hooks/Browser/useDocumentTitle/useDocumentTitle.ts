6b89b922e34e369bd303f2d1258c48ec44e0c552
Lystrillian <webeugene@proton.me>
2024-06-20 01:45:52 +0300

    [chore]: useDocumentTitle formatted and linted

--- hooks/Browser/useDocumentTitle/useDocumentTitle.ts
+++ hooks/Browser/useDocumentTitle/useDocumentTitle.ts
@@ -1,22 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
 interface UseDocumentTitleOptions {
-  initialTitle?: string
-  formatter?: (title: string) => string
+  initialTitle?: string;
+  formatter?: (title: string) => string;
 }
 
 export function useDocumentTitle(
   title: string,
   options: UseDocumentTitleOptions = {}
 ) {
-  const { initialTitle, formatter } = options
+  const { initialTitle, formatter } = options;
 
   useEffect(() => {
-    const formattedTitle = formatter ? formatter(title) : title
+    const formattedTitle = formatter ? formatter(title) : title;
     if (formattedTitle) {
-      document.title = formattedTitle
+      document.title = formattedTitle;
     } else if (initialTitle) {
-      document.title = initialTitle
+      document.title = initialTitle;
     }
-  }, [title, initialTitle, formatter])
+  }, [title, initialTitle, formatter]);
 }
