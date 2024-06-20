49187eb7bdad439afb67cae77b0492cd319af232
Lystrillian <webeugene@proton.me>
2024-06-20 04:43:00 +0300

    [docs]: updated page for useInterval and for useIntervalFn

--- hooks/Animation/useInterval/useInterval.demo.tsx
+++ hooks/Animation/useInterval/useInterval.demo.tsx
@@ -8,25 +8,25 @@ export default function IntervalDemo() {
   });
 
   return (
-    <div className="not-prose mb-[10px] rounded-[8px] transition-colors bg-black p-8 border rounded-lg py-4 px-8">
-      <div className="text-[12px] font-medium leading-[28px] margin-[0.5rem]  text-[rgba(235,235,245,.6)]">
-        Interal Fired: {counter}
+    <div className="border rounded-lg p-[2em] relative mb-[10px] transition-colors">
+      <div>
+        Interval Fired: {counter}
       </div>
-      <div className="flex space-x-4">
+      <div className="flex my-4 gap-2">
         <button
-          className="inline-flex items-center justify-center font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 rounded-md px-3 text-xs gap-1.5"
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
           onClick={reset}
         >
           Reset
         </button>
         <button
-          className="inline-flex items-center justify-center font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 rounded-md px-3 text-xs gap-1.5"
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
           onClick={pause}
         >
           Pause
         </button>
         <button
-          className="inline-flex items-center justify-center font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 rounded-md px-3 text-xs gap-1.5"
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
           onClick={resume}
         >
           Resume
