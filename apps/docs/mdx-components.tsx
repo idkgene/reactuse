8752646a7da4d149344d4c9cb6dc4d0296648a41
Lystrillian <webeugene@proton.me>
2024-07-04 05:41:57 +0300

    chore: deleting stuff

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,7 +1,6 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
 import Demo from './components/Common/Demo/demo';
-import Code from './components/Common/Code/code';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -20,6 +19,5 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {children}
       </Demo>
     ),
-    Code: ({ filePath }: { filePath: string }) => <Code filePath={filePath} />,
   };
 }
