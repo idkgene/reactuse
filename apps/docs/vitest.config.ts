2c2aacc26063b54a003e203833c55a955ad04582
Lystrillian <webeugene@proton.me>
2024-07-08 18:04:22 +0300

    deps update + eslint ignore added + vitest config

--- apps/docs/vitest.config.ts
+++ apps/docs/vitest.config.ts
@@ -2,9 +2,37 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: {
-      reporter: ['text', 'clover', 'json'],
-    },
+    globals: true,
     environment: 'jsdom',
+    exclude: ['node_modules', 'dist', 'build'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        'build/**',
+        '**/*.d.ts',
+        'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*.{test,spec}.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc}.config.*',
+        '**/node_modules/**',
+      ],
+    },
+    alias: {
+      '~': './hooks/',
+    },
+    allowOnly: false,
+    bail: 1,
+    clearMocks: true,
+    maxConcurrency: 10,
+    minWorkers: 1,
+    maxWorkers: 4,
+    mockReset: true,
+    silent: false,
+    fileParallelism: true,
+    watch: false,
+    testTimeout: 5000,
+    hookTimeout: 10000,
   },
 });
