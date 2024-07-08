4fd72d4cfac02ddd4f791cec8625a344566e1c36
Lystrillian <webeugene@proton.me>
2024-07-08 18:52:19 +0300

    ui: demo component refactored

--- apps/docs/components/Common/Demo/demo.tsx
+++ apps/docs/components/Common/Demo/demo.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import GithubIcon from '../Icons/github-icon';
 
 interface DemoProps {
   children: React.ReactNode;
@@ -20,15 +21,18 @@ export default function Demo({
 
   const demoFile = `${kebabTitle}.demo.tsx`;
   const link = `${baseUrl}/${category}/${title}/${demoFile}`;
-
   return (
     <div className="bg-card relative flex flex-col rounded-xl border p-[2em]">
-      <Link
-        href={link}
-        className="absolute right-3 top-3 text-sm font-semibold"
-      >
-        source
-      </Link>
+      <div className="absolute right-3 top-3 flex gap-1 text-sm font-semibold">
+        <button
+          type="button"
+          className="focus-visible:ring-ring border-input bg-background text-foreground hover:bg-muted hover:text-foreground relative z-10 inline-flex size-7 items-center justify-center whitespace-nowrap rounded-md border text-sm font-medium opacity-100 shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-3.5"
+        >
+          <Link href={link}>
+            <GithubIcon />
+          </Link>{' '}
+        </button>
+      </div>
       <div>{children}</div>
     </div>
   );
