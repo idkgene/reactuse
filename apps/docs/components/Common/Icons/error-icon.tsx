894d0f958ed36ce7d95eb4e5d2bb4344a3cc15a1
Lystrillian <webeugene@proton.me>
2024-07-08 18:04:45 +0300

    ui: added some icons as components

---
+++ apps/docs/components/Common/Icons/error-icon.tsx
@@ -0,0 +1,19 @@
+export default function ErrorIcon(): JSX.Element {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 5.66669V9.00002M9 12.3334H9.00834M5.55001 0.666687H12.45L17.3333 5.55002V12.45L12.45 17.3334H5.55001L0.666672 12.45V5.55002L5.55001 0.666687Z"
+        stroke="#FF6166"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
