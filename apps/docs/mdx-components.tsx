b2669a095954754ac2e797405853b4ec2c48c36e
Lystrillian <webeugene@proton.me>
2024-07-16 01:08:40 +0300

    chore: deps update storybook set up

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,11 +1,14 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
+import { Card, Cards } from 'fumadocs-ui/components/card';
 import Demo from './components/Common/Demo/demo';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    Card,
+    Cards,
     Demo: ({
       category,
       title,
