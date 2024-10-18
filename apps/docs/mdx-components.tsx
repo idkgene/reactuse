838e2aadf5e331267ce0158f801ce2ec368ef06d
Eugene <webeugene@proton.me>
2024-10-18 10:48:10 +0300

    Removed no longer used component from mdx-components file

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,7 +1,6 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
 import { Card, Cards } from 'fumadocs-ui/components/card';
-import Demo from './components/Common/Demo/demo';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -9,18 +8,5 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ...components,
     Card,
     Cards,
-    Demo: ({
-      category,
-      title,
-      children,
-    }: {
-      category: string;
-      title: string;
-      children: React.ReactNode;
-    }) => (
-      <Demo category={category} title={title}>
-        {children}
-      </Demo>
-    ),
   };
 }
