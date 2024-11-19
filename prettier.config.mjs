5f108be8e5f9c0f26a73a6105bdcd38aa6abfef1
idkgene <webeugene@proton.me>
2024-11-19 15:09:08 +0300

    chore(settings): removed prettier config file in favor of package.json configuration

--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-import prettierOptions from '@reactuse/prettier-config'
-
-/** @type {import("prettier").Config} */
-const config = {
-  ...prettierOptions,
-  plugins: [...prettierOptions.plugins],
-};
-
-export default config;
